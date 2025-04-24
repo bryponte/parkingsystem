fb8a267b94277a949a2e22a530f2a6c13503f640
Bryan Regor S. Ponte <bryanregor.ponte@gmail.com>
2025-04-24 11:07:21 +0800

    changing title

--- park.php
+++ park.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parking System</title>
+    <title>Ponte System</title>
 </head>
 <body>
     <a href="addParking.php">Park Vehicle</a>
